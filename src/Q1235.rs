a50d08db390feec5fcebedf37913b463b6e3a481
QASAK <w663012911@outlook.com>
2022-10-23 17:33:13 +0800

    Update Q1235.rs

--- src/Q1235.rs
+++ src/Q1235.rs
@@ -1,7 +1,8 @@
 pub fn job_scheduling(start_time: Vec<i32>, end_time: Vec<i32>, profit: Vec<i32>) -> i32 {
     let n = start_time.len();
-    let mut jobs = (0..n).map(|i| vec![start_time[i], end_time[i], profit[i], profit[i]]).collect::<Vec<_>>();
+    let mut jobs = (0..n).map(|i| vec![start_time[i], end_time[i], profit[i]]).collect::<Vec<_>>();
     jobs.sort_by(|a, b| a[1].cmp(&b[1]));
+    let mut f = (0..n).map(|i| jobs[i][2]).collect::<Vec<_>>();
     let mut ret = 0;
     // 大于等于x的最小值[)
     // 小于等于x的最大值(] √
@@ -21,11 +22,13 @@ pub fn job_scheduling(start_time: Vec<i32>, end_time: Vec<i32>, profit: Vec<i32>
         if l == -1 {None} else {Some(l as usize)}
     }
     for i in 1..n {
-        jobs[i][3] = jobs[i][3].max(jobs[i - 1][3]);
+        // not select job i
+        f[i] = f[i].max(f[i - 1]);
+        // select job i，and find most close but not cross job j
         if let Some(j) = bs(&jobs, i) {
-            jobs[i][3] = jobs[i][3].max(jobs[j as usize][3] + jobs[i][2]);
+            f[i] = f[i].max(f[j] + jobs[i][2]);
         }
-        ret = ret.max((jobs[i][3]));
+        ret = ret.max(f[i]);
     }
     ret
 }
