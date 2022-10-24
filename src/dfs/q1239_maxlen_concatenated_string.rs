6da573c91dbef156d5b7042362d89be0399e06bf
QASAK <w663012911@outlook.com>
2022-10-24 18:54:13 +0800

    Update q1239_maxlen_concatenated_string.rs
    add bitwise way

--- src/dfs/q1239_maxlen_concatenated_string.rs
+++ src/dfs/q1239_maxlen_concatenated_string.rs
@@ -1,13 +1,16 @@
 // leetcode 2022-10-24 EN
+
+// 1. directly use string to count
+// 2. convert valid string to int with bitwise op, then count
 pub fn max_length(arr: Vec<String>) -> i32 {
-    // max_length from index i
+    // use String, in each level of recursion, we have to clone it
     fn dfs(i: usize, cur: String,  arr: &Vec<String>) -> i32 {
         if i == arr.len() {
             return cur.len() as i32;
         }
         let mut a = 0;
         let mut b = 0;
-        // to i , choose or not choose
+        // to item at index i, we can choose or not to choose
         let s = cur.clone() + arr[i].as_str();
         if check(&s) {
             a = dfs(i + 1, s, arr);
@@ -28,4 +31,31 @@ pub fn max_length(arr: Vec<String>) -> i32 {
         true
     }
     dfs(0, "".to_string(), &arr)
+}
+
+
+pub fn max_length_bit(arr: Vec<String>) -> i32 {
+    let mut ret: u32 = 0;
+    let mut masks: Vec<u32> = vec![0];
+    for s in arr {
+        // convert valid strings to bit masks
+        let mut mask: u32 = s.bytes().map(|c| 1<<(c-b'a')).sum();
+        if mask.count_ones() == s.len() as u32 {
+            masks.push(mask); 
+        } else {
+            continue;
+        }
+        // combine current bit mask to existed masks, then count the combined one's len,
+        // push it into our vec
+        // compare with current max length
+        let n = masks.len();
+        for i in 0..(n - 1) {
+            let m = masks[i];
+            if (m & mask).count_ones() == 0 {
+                masks.push(m | mask);
+                ret = ret.max((m | mask).count_ones());
+            }
+        }
+    }
+    ret as i32
 }
