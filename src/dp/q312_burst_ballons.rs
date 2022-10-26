ee92bbce9fa520ea3666fcb21daa388940e8bd1d
QASAK <w663012911@outlook.com>
2022-10-26 18:19:48 +0800

    Update q312_burst_ballons.rs

--- src/dp/q312_burst_ballons.rs
+++ src/dp/q312_burst_ballons.rs
@@ -1,3 +1,24 @@
+// cached backtrack
 pub fn max_coins(nums: Vec<i32>) -> i32 {
-    0
+    fn dfs(l: usize, r: usize, nums: &Vec<i32>, f: &mut Vec<Vec<i32>>) -> i32 {
+        if l + 1 >= r {
+            return 0;
+        }
+        if f[l][r] != -1 {
+            return f[l][r];
+        }
+        for i in (l + 1)..=(r - 1) {
+            f[l][r] = f[l][r].max(nums[i] * nums[l] * nums[r] + dfs(l, i, nums, f) + dfs(i, r, nums, f));
+        }
+        return f[l][r];
+    }
+    let n = nums.len();
+    let mut v = vec![0; n + 2];
+    for i in 1..=n {
+        v[i] = nums[i - 1];
+    }
+    v[0] = 1;
+    v[n + 1] = 1;
+    let mut f = vec![vec![-1; n + 2]; n + 2];
+    dfs(0, n + 1, &v, &mut f)
 }
