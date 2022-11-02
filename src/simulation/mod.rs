dfe3bf5bc846a3238a756e33c8bc42e2cb68ee5e
QASAK <w663012911@outlook.com>
2022-11-02 23:57:15 +0800

    q1620

--- src/simulation/mod.rs
+++ src/simulation/mod.rs
@@ -4,3 +4,4 @@ mod q835_image_overlap;
 mod q766_diagonal_order_access_matrix;
 mod q1662_array_string_equal;
 mod q1706_find_ball;
+mod q1620_best_coordinate;
---
+++ src/simulation/q1620_best_coordinate.rs
@@ -0,0 +1,35 @@
+pub fn best_coordinate(towers: Vec<Vec<i32>>, radius: i32) -> Vec<i32> {
+    let mut max = 0;
+    let mut ret = vec![0; 2];
+    let n = towers.len();
+    for x in 0..=50 {
+        for y in 0..=50 {
+            let mut cur = 0;
+            for j in 0..n {
+                let tr = &towers[j];
+                let xx = tr[0]; let yy = tr[1];
+                let signal = tr[2];
+                let d = (((x - xx) * (x - xx) + (y - yy) * (y - yy)) as f32).sqrt();
+                if d <= (radius as f32) {
+                    let w = signal / (1 + (d as i32));
+                    cur += w;
+                }
+            }
+            if x == 24 && y == 45 {
+                println!("{cur}, {max}");
+            }
+            if cur > max {
+                max = cur;
+                ret[0] = x; ret[1] = y;
+            }
+            if cur == max {
+                if x < ret[0] {
+                    ret[0] = x; ret[1] = y;
+                } else if x == ret[0] && y < ret[1] {
+                    ret[0] = x; ret[1] = y;
+                }
+            }
+        }
+    }
+    ret
+}
