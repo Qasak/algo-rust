76322e08e7a35c94c47b7490bff959ca023a6deb
QASAK <w663012911@outlook.com>
2022-11-04 18:38:02 +0800

    add q754

--- src/greedy/mod.rs
+++ src/greedy/mod.rs
@@ -1,1 +1,2 @@
 mod q2131_longest_palindrome;
+mod q754_reach_number;
---
+++ src/greedy/q754_reach_number.rs
@@ -0,0 +1,9 @@
+pub fn reach_number(target: i32) -> i32 {
+    let mut diff = target.abs();
+    let mut step = 0;
+    while diff > 0 {
+        step += 1;
+        diff -= step;
+    }
+    if diff % 2 == 0 {step} else {step + 1 + step % 2}
+}
