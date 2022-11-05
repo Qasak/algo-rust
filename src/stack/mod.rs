f1959ac0f454a6780c1691173d9b846cfb10cc78
QASAK <w663012911@outlook.com>
2022-11-05 14:59:47 +0800

    add q1106

--- src/stack/mod.rs
+++ src/stack/mod.rs
@@ -1,2 +1,3 @@
 mod q901_stock_spanner;
 mod q907_sum_subarray_mins;
+mod q1106_parse_bool_expr;
---
+++ src/stack/q1106_parse_bool_expr.rs
@@ -0,0 +1,44 @@
+pub fn parse_bool_expr(expression: String) -> bool {
+    let mut stk = vec![];
+    for ch in expression.chars() {
+        if ch == ',' {
+            continue;
+        }
+        if ch != ')' {
+            stk.push(ch);
+            continue;
+        }
+        // ch == ')'
+        let (mut f, mut t) = (0, 0);
+        while let Some(val) = stk.pop() {
+            if val == '(' {break;}
+            if val == 'f' {f += 1} else {t += 1};
+        }
+        if let Some(exp) = stk.pop() {
+            if exp == '!' {
+                stk.push(if t == 1 {'f'} else {'t'});
+            } else if exp == '&' {
+                stk.push(if f >= 1 {'f'} else {'t'});
+            } else {
+                stk.push(if t >= 1 {'t'} else {'f'});
+            }
+        }
+    }
+    if let Some(val) = stk.pop() {
+        if val == 't' {true} else {false}
+    } else {
+        false
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::stack::q1106_parse_bool_expr::parse_bool_expr;
+
+    #[test]
+    fn t() {
+        assert_eq!(false, parse_bool_expr("&(|(f))".to_string()));
+        assert_eq!(true, parse_bool_expr("|(f,f,f,t)".to_string()));
+        assert_eq!(true, parse_bool_expr("!(&(f,t))".to_string()));
+    }
+}
