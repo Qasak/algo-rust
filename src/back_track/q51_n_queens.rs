4fb4037061bb3ae818a517155db02d39ddc1998b
QASAK <w663012911@outlook.com>
2022-11-08 02:57:40 +0800

    Update q51_n_queens.rs
    add backtrack + bitwise solution

--- src/back_track/q51_n_queens.rs
+++ src/back_track/q51_n_queens.rs
@@ -22,3 +22,27 @@ pub fn solve_n_queens(n: i32) -> Vec<Vec<String>> {
     ret
 }
 
+// bit_wise
+pub fn solve_n_queens_1(n: i32) -> Vec<Vec<String>> {
+    fn dfs(ret: &mut Vec<Vec<String>>, path: &mut Vec<Vec<char>>, col: i32, d1: i32, d2: i32, i: usize) {
+        let n = path.len();
+        if i == n {
+            ret.push(path.iter().map(|v| v.iter().map(|&c| c).collect::<String>()).collect::<Vec<String>>());
+        } else {
+            let mut pos = ((1 << n) - 1) & (!(col|d1|d2));
+            while pos != 0 {
+                let low_bit = pos & (-pos);
+                pos = pos & (pos - 1);
+                let j = (low_bit - 1).count_ones() as usize;
+                path[i][j] = 'Q';
+                dfs(ret, path, col|low_bit, (d1|low_bit) << 1, (d2|low_bit) >> 1, i + 1);
+                path[i][j] = '.';
+            }
+        }
+    }
+    let n = n as usize;
+    let mut ret = vec![];
+    let mut path = vec![vec!['.'; n]; n];
+    dfs(&mut ret, &mut path, 0, 0, 0, 0);
+    ret
+}
