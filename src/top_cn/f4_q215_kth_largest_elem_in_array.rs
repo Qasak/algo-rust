21d61886d6acda78dff04760693fbed6d16735c3
QASAK <w663012911@outlook.com>
2022-11-09 00:04:28 +0800

    add q215
    qss first edition

---
+++ src/top_cn/f4_q215_kth_largest_elem_in_array.rs
@@ -0,0 +1,73 @@
+use rand::Rng;
+
+pub fn find_kth_largest(mut nums: Vec<i32>, k: i32) -> i32 {
+    fn qss(arr: &mut Vec<i32>, l: usize, r: usize, k: usize) {
+        if l >= r {
+            return;
+        }
+        let (le, eq) = partition(arr, l, r);
+        if k as i32 <= (le as i32 - 1) {
+            qss(arr, l, le - 1, k);
+        }
+        if k >= eq + 1 {
+            qss(arr, eq + 1, r, k);
+        }
+    }
+    fn partition(arr: &mut Vec<i32>, l: usize, r: usize) -> (usize, usize) {
+        let mut rng = rand::thread_rng();
+        // if use rand 0.8.5
+        // let pick = rng.gen_range(l..r);
+        let pick = rng.gen_range(l, r + 1);
+        let pivot = arr[pick];
+        swap(arr, pick, r);
+        // le: next pos of less/ first item equal to pivot
+        // eq: next pos of eq/ first item greater to pivot
+        // gt: left next pos of gt/ last item equal to pivot
+        let (mut le, mut eq, mut gt) = (l, l, r - 1);
+        while gt > 0 && eq <= gt {
+            if arr[eq] == pivot {
+                eq += 1;
+            } else if arr[eq] < pivot {
+                swap(arr, eq, le);
+                eq += 1; le += 1;
+            } else {
+                swap(arr, eq, gt);
+                if gt == 0 {
+                    break;
+                }
+                gt -= 1;
+            }
+        }
+
+        swap(arr, eq, r);
+        (le, eq)
+
+    }
+    fn swap(arr: &mut Vec<i32>, i: usize, j: usize) {
+        let t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
+    let k = k as usize;
+    let n = nums.len();
+    qss(&mut nums, 0, n - 1, n - k);
+    nums[n - k]
+}
+
+#[cfg(test)]
+mod test{
+    use crate::top_cn::f4_q215_kth_largest_elem_in_array::find_kth_largest;
+
+    #[test]
+    fn f() {
+        for i in 0..20 {
+            println!("{}", find_kth_largest([3,2,13,2,3,3,2,13,2,3,1,2,4,5,5,6,5,6,43,2,13,2,3,1,2,4,5,5,6,5,6,41,2,4,5,5,6,5,6,4].to_vec(), 11));
+        }
+        for i in 0..20 {
+            println!("{}", find_kth_largest([3,2,13,2,3,1,2,4,53,2,13,2,3,3,2,13,2,3,1,2,4,5,5,6,5,6,43,2,13,2,3,1,2,4,5,5,6,5,6,41,2,4,5,5,6,5,6,43,2,13,2,3,3,2,13,2,3,1,2,4,5,5,6,5,6,43,2,13,2,3,1,2,4,5,5,6,5,6,41,2,4,5,5,6,5,6,4,5,6,5,6,4].to_vec(), 1));
+        }
+        for i in 0..20 {
+            println!("{}", find_kth_largest([2,1].to_vec(), 1));
+        }
+    }
+}
--- src/top_cn/mod.rs
+++ src/top_cn/mod.rs
@@ -1,3 +1,4 @@
 mod f1_q3_longest_substring;
 pub mod f2_q206_reverse_list;
 mod f3_q146_lru;
+mod f4_q215_kth_largest_elem_in_array;
