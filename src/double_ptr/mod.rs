969aefe2cc8b6f7187d4359b718624289ddb9a03
QASAK <w663012911@outlook.com>
2022-11-15 21:42:37 +0800

    add q15 double ptr

--- src/double_ptr/mod.rs
+++ src/double_ptr/mod.rs
@@ -1,3 +1,4 @@
 mod q481_magic_string;
 mod q26_remove_dup_from_sorted_arr;
 mod q11_max_area;
+mod q15_three_sum;
---
+++ src/double_ptr/q15_three_sum.rs
@@ -0,0 +1,24 @@
+pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
+    let mut arr = nums;
+    let mut ret = vec![];
+    arr.sort();
+    let (mut i, n) = (0, arr.len());
+    while i < n {
+        let (mut l, mut r, target) = (i + 1, n - 1, -arr[i]);
+        while l < r {
+            if arr[l] + arr[r] == target {
+                ret.push(vec![arr[i], arr[l], arr[r]]);
+                l += 1; r -= 1;
+                while l < r && arr[l] == arr[l - 1] {l += 1;}
+                while l < r && arr[r] == arr[r + 1] {r -= 1;}
+            } else if arr[l] + arr[r] < target {
+                l += 1;
+            } else {
+                r -= 1;
+            }
+        }
+        i += 1;
+        while i < n && arr[i] == arr[i - 1] {i += 1;}
+    }
+    ret
+}
