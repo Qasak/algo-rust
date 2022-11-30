7c7c9e352d17d9e1e23ed0e117f335aade9b2cc9
QASAK <w663012911@outlook.com>
2022-12-01 01:54:10 +0800

    Update q895_freq_stack.rs
    add hashmap + bucket stacks solution

---
+++ src/stack/q895_freq_stack.rs
@@ -0,0 +1,36 @@
+use std::collections::{HashMap, VecDeque};
+struct FreqStack {
+    // val: freq
+    map: HashMap<i32, usize>,
+    // stacks
+    stacks: Vec<VecDeque<i32>>
+}
+
+impl FreqStack {
+
+    fn new() -> Self {
+        FreqStack{
+            map: HashMap::new(),
+            stacks: Vec::new(),
+        }
+    }
+
+    fn push(&mut self, val: i32) {
+        *self.map.entry(val).or_insert(0) += 1;
+        let freq = self.map[&val];
+        if freq > self.stacks.len() {
+            self.stacks.push(VecDeque::new());
+        }
+        self.stacks[freq - 1].push_back(val);
+    }
+
+    fn pop(&mut self) -> i32 {
+        let n = self.stacks.len();
+        let val = self.stacks[n - 1].pop_back().unwrap();
+        *self.map.entry(val).or_insert(0) -= 1;
+        if self.stacks.last().unwrap().is_empty() {
+            self.stacks.pop();
+        }
+        val
+    }
+}
