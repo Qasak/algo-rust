6e997e9fb1b00bc4ea6add8ca2921650786a4751
QASAK <w663012911@outlook.com>
2022-12-01 20:23:22 +0800

    Update q2225_find_winners.rs

--- src/simulation/q2225_find_winners.rs
+++ src/simulation/q2225_find_winners.rs
@@ -20,20 +20,14 @@ pub fn find_winners(matches: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
 
 // iter soltion
 pub fn find_winners_1(matches: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-    use std::collections::HashMap;
-    impl Solution {
-        pub fn find_winners(matches: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-            // member: defeated_times
-            let map:HashMap<i32, i32> = matches.iter().
-                fold(HashMap::new(), |mut m, p| {
-                    *m.entry(p[0]).or_default() += 0;
-                    *m.entry(p[1]).or_default() += 1;
-                    m
-                });
-            let mut a: Vec<i32> = map.iter().filter(|e| *e.1 == 0).map(|e| *e.0).collect();
-            let mut b: Vec<i32> = map.iter().filter(|e| *e.1 == 1).map(|e| *e.0).collect();
-            a.sort(); b.sort();
-            vec![a, b]
-        }
-    }
+    let map:HashMap<i32, i32> = matches.iter().
+        fold(HashMap::new(), |mut m, p| {
+            *m.entry(p[0]).or_default() += 0;
+            *m.entry(p[1]).or_default() += 1;
+            m
+        });
+    let mut a: Vec<i32> = map.iter().filter(|e| *e.1 == 0).map(|e| *e.0).collect();
+    let mut b: Vec<i32> = map.iter().filter(|e| *e.1 == 1).map(|e| *e.0).collect();
+    a.sort(); b.sort();
+    vec![a, b]
 }
