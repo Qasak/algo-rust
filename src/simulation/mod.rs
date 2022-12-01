655d8a5ce7867a7bd2458b5390544bf1d5200666
QASAK <w663012911@outlook.com>
2022-12-01 20:05:20 +0800

    add q2225

--- src/simulation/mod.rs
+++ src/simulation/mod.rs
@@ -12,3 +12,4 @@ mod q1752_check_reverse_arr;
 mod q1758_min_operations;
 mod q1779_nearest_valid_point;
 mod q1704_havles_are_alike;
+mod q2225_find_winners;
---
+++ src/simulation/q2225_find_winners.rs
@@ -0,0 +1,22 @@
+use std::collections::HashSet;
+impl Solution {
+    pub fn find_winners(matches: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        let mut cnt = vec![0; 100001];
+        let mut members = HashSet::new();
+        for m in matches {
+            cnt[m[1] as usize] += 1;
+            members.insert(m[1] as usize); members.insert(m[0] as usize);
+        }
+        let (mut ret, mut a, mut b) = (vec![], vec![], vec![]);
+        for i in members {
+            if cnt[i] == 0 {
+                a.push(i as i32);
+            } else if cnt[i] == 1 {
+                b.push(i as i32);
+            }
+        }
+        a.sort(); b.sort();
+        ret.push(a); ret.push(b);
+        ret
+    }
+}
