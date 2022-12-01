44af9df65e13a6a6eb7e7fd2c41fc75e537c4753
QASAK <w663012911@outlook.com>
2022-12-01 17:53:34 +0800

    Update q1704_havles_are_alike.rs
    simplify: &[u8] -> &str

--- src/simulation/q1704_havles_are_alike.rs
+++ src/simulation/q1704_havles_are_alike.rs
@@ -1,18 +1,16 @@
 pub fn halves_are_alike(s: String) -> bool {
     let n = s.len();
-    let a = &s.as_bytes()[..(n / 2)];
-    let b = &s.as_bytes()[(n / 2)..n];
-    fn cnt(s :&[u8]) -> i32 {
+    fn cnt(s :&str) -> i32 {
         let vowels = "aeiouAEIOU";
         let mut ret = 0;
-        for c in s {
-            if vowels.contains(*c as char) {
+        for c in s.chars() {
+            if vowels.contains(c) {
                 ret += 1;
             }
         }
         ret
     }
-    cnt(a) == cnt(b)
+    cnt(&s[..(n / 2)]) == cnt(&s[(n / 2)..n])
 }
 
 
