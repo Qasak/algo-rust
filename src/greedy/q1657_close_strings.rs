a5a215ae62147e5bb2400bcf86304f60601dbb71
QASAK <w663012911@outlook.com>
2022-12-02 18:10:03 +0800

    Update q1657_close_strings.rs
    another good solution

--- src/greedy/q1657_close_strings.rs
+++ src/greedy/q1657_close_strings.rs
@@ -15,3 +15,21 @@ pub fn close_strings(word1: String, word2: String) -> bool {
     cnt1.0.sort(); cnt2.0.sort();
     cnt1 == cnt2
 }
+
+// https://leetcode.com/problems/determine-if-two-strings-are-close/solutions/1029085/rust-solution/
+pub fn close_strings_1(word1: String, word2: String) -> bool {
+    let mut counts1 = [0; 26];
+    let mut counts2 = [0; 26];
+    for &b in word1.as_bytes() {
+        counts1[(b - b'a') as usize] += 1;
+    }
+    for &b in word2.as_bytes() {
+        counts2[(b - b'a') as usize] += 1;
+    }
+    if (0..26).any(|i| (counts1[i] > 0) != (counts2[i] > 0)) {
+        return false;
+    }
+    counts1.sort_unstable();
+    counts2.sort_unstable();
+    counts1 == counts2
+}
