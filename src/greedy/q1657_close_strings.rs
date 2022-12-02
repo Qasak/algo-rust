fa4359416edbdd82ab582b924ebe81fbd37b5684
QASAK <w663012911@outlook.com>
2022-12-02 18:05:28 +0800

    simplify

--- src/greedy/q1657_close_strings.rs
+++ src/greedy/q1657_close_strings.rs
@@ -1,22 +1,17 @@
+// https://github.com/Qasak/algo-rust
 pub fn close_strings(word1: String, word2: String) -> bool {
-    let (mut cnt1, mut cnt2, k1, k2) = (
-        word1.bytes().fold(vec![0; 26], |mut cnt, b| {
-            cnt[(b - b'a') as usize] += 1;
+    let (mut cnt1, mut cnt2) = (
+        word1.bytes().fold((vec![0; 26], vec![false; 26]), |mut cnt, b| {
+            cnt.0[(b - b'a') as usize] += 1;
+            cnt.1[(b - b'a') as usize] = true;
             cnt
         }),
-        word2.bytes().fold(vec![0; 26], |mut cnt, b| {
-            cnt[(b - b'a') as usize] += 1;
+        word2.bytes().fold((vec![0; 26], vec![false; 26]), |mut cnt, b| {
+            cnt.0[(b - b'a') as usize] += 1;
+            cnt.1[(b - b'a') as usize] = true;
             cnt
-        }),
-        word1.bytes().fold(vec![false; 26], |mut k, b| {
-            k[(b - b'a') as usize] = true;
-            k
-        }),
-        word2.bytes().fold(vec![false; 26], |mut k, b| {
-            k[(b - b'a') as usize] = true;
-            k
         })
     );
-    cnt1.sort(); cnt2.sort();
-    cnt1 == cnt2 && k1 == k2
+    cnt1.0.sort(); cnt2.0.sort();
+    cnt1 == cnt2
 }
