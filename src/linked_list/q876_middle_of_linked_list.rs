c3499e5c016aa32b47d107aea8f9558f916334b7
QASAK <w663012911@outlook.com>
2022-12-06 03:20:43 +0800

    Update q876_middle_of_linked_list.rs
    add two solutions

--- src/linked_list/q876_middle_of_linked_list.rs
+++ src/linked_list/q876_middle_of_linked_list.rs
@@ -50,3 +50,36 @@ pub fn middle_node_1(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
     // .clone() makes &Option<Box<ListNode>> to Option<Box<ListNode>>
     slow.clone()
 }
+
+pub fn middle_node_2(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    // 使用快慢指针来遍历链表
+    let mut fast = &head;
+    let mut slow = &head;
+    while let Some(node) = fast {
+        fast = &node.next;
+        if let Some(node) = fast {
+            fast = &node.next;
+            slow = &slow.as_ref().unwrap().next;
+        }
+    }
+    slow.clone()
+}
+
+pub fn middle_node_3(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    // 首先遍历链表，计算链表的长度
+    let mut len = 0;
+    let mut p = &head;
+    while let Some(node) = p {
+        len += 1;
+        p = &node.next;
+    }
+
+    // 然后遍历链表的前一半，直到找到中间结点
+    let mut mid = len / 2;
+    p = &head;
+    while mid > 0 {
+        p = &p.as_ref().unwrap().next;
+        mid -= 1;
+    }
+    p.clone()
+}
