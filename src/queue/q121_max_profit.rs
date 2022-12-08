5707f09e405345f0444ce7dd1a3ca134b2390764
QASAK <w663012911@outlook.com>
2022-12-08 20:51:38 +0800

    Update q121_max_profit.rs
    add dp solution

--- src/queue/q121_max_profit.rs
+++ src/queue/q121_max_profit.rs
@@ -14,3 +14,25 @@ pub fn max_profit(prices: Vec<i32>) -> i32 {
     ret = ret.max(*q.back().unwrap() - *q.front().unwrap());
     ret
 }
+
+// dp solution
+pub fn max_profit_1(prices: Vec<i32>) -> i32 {
+    let (mut f, mut min, n) = (vec![0; prices.len()], prices[0], prices.len());
+    for i in 1..n {
+        f[i] = f[i - 1].max(prices[i] - min);
+        min = min.min(prices[i]);
+    }
+    f[n - 1]
+}
+
+// optimization dp
+pub fn max_profit_2(prices: Vec<i32>) -> i32 {
+    let (mut ret, mut min) = (0, i32::MAX);
+    for p in prices {
+        min = min.min(p);
+        ret = ret.max(p - min);
+    }
+    ret
+}
+
+//
