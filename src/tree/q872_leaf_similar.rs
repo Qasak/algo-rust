27aae34b618769bf915054ba46e1ba40c9025617
QASAK <w663012911@outlook.com>
2022-12-08 19:57:23 +0800

    Update q872_leaf_similar.rs
    2 ways: rec and iter

---
+++ src/tree/q872_leaf_similar.rs
@@ -0,0 +1,49 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+use crate::tree::q222_count_complete_tree_nodes::TreeNode;
+
+type T = Option<Rc<RefCell<TreeNode>>>;
+
+// iterative
+pub fn leaf_similar(root1: T, root2: T) -> bool {
+    Solution::iterative(root1) == Solution::iterative(root2)
+}
+fn iterative(root: T) -> Vec<i32> {
+    let mut stack = vec![root];
+    let mut leaf  = vec![];
+
+    while let Some(node) = stack.pop() {
+        if let Some(node) = node {
+            let node = node.borrow();
+            if node.left.is_none() && node.right.is_none() {
+                leaf.push(node.val);
+            }
+            else {
+                stack.push(node.right.clone());
+                stack.push(node.left.clone());
+            }
+        }
+    }
+    leaf
+}
+
+// recursive
+pub fn leaf_similar_1(root1: T, root2: T) -> bool {
+    fn get_leafs(root: &T, arr: &mut Vec<i32>) {
+        if let Some(node) = root {
+            let node = node.borrow();
+            if !node.left.is_some() && !node.right.is_some() {
+                arr.push(node.val);
+            } else {
+                get_leafs(&node.left, arr);
+                get_leafs(&node.right, arr);
+            }
+        }
+    }
+    let mut arr1 = vec![];
+    let mut arr2 = vec![];
+    get_leafs(&root1, &mut arr1);
+    get_leafs(&root2, &mut arr2);
+    arr1 == arr2
+}
+
