fbe2094a6a61f3445bd5691d8e4fa57b17393ca9
QASAK <w663012911@outlook.com>
2022-12-08 18:09:06 +0800

    Update q938_range_sum_bst.rs

--- src/tree/q938_range_sum_bst.rs
+++ src/tree/q938_range_sum_bst.rs
@@ -3,20 +3,23 @@ use std::cell::RefCell;
 use crate::tree::q222_count_complete_tree_nodes::TreeNode;
 
 type T = Option<Rc<RefCell<TreeNode>>>;
-impl Solution {
-    pub fn range_sum_bst(root: T, low: i32, high: i32) -> i32 {
-        fn dfs(root: &T, low: i32, high: i32) -> i32{
-            if let Some(node) = root {
-                let node = node.borrow();
-                let cur = node.val;
-                let a = if cur < low || cur > high {0} else {cur};
-                let b = if cur == high {0} else {dfs(&node.right, low, high)};
-                let c = if cur == low {0} else {dfs(&node.left, low, high)};
-                a + b + c
-            } else {
-                0
-            }
+
+
+// straight dfs
+pub fn range_sum_bst(root: T, low: i32, high: i32) -> i32 {
+    fn dfs(root: &T, low: i32, high: i32) -> i32{
+        if let Some(node) = root {
+            let node = node.borrow();
+            let cur = node.val;
+            let a = if cur < low || cur > high {0} else {cur};
+            let b = if cur == high {0} else {dfs(&node.right, low, high)};
+            let c = if cur == low {0} else {dfs(&node.left, low, high)};
+            a + b + c
+        } else {
+            0
         }
-        dfs(&root, low, high)
     }
+    dfs(&root, low, high)
 }
+
+
