471157426916328b3e0386c56d882b4e13e21b78
QASAK <w663012911@outlook.com>
2022-12-19 23:06:36 +0800

    Update q1971_valid_path.rs
    add UF solution

---
+++ src/graph/q1971_valid_path.rs
@@ -0,0 +1,39 @@
+struct UF {
+    p: Vec<usize>
+}
+
+impl UF {
+    fn new(n: usize) -> Self {
+        let p = (0..=n).collect();
+        UF {p}
+    }
+
+    fn find(&mut self, u: usize) -> usize {
+        if self.p[u] == u {
+            return u;
+        } else {
+            let pu = self.find(self.p[u]);
+            self.p[u] = pu;
+            return self.p[u];
+        }
+    }
+
+    fn union(&mut self, u: usize, v: usize) {
+        if self.p[u] == self.p[v] {
+            return;
+        } else {
+            let (pu, pv) = (self.find(self.p[u]), self.find(self.p[v]));
+            self.p[pu] = pv;
+        }
+    }
+}
+
+
+pub fn valid_path(n: i32, edges: Vec<Vec<i32>>, source: i32, destination: i32) -> bool {
+    let n = n as usize;
+    let mut UF = UF::new(n);
+    for e in edges {
+        UF.union(e[0] as usize, e[1] as usize);
+    }
+    UF.find(source as usize) == UF.find(destination as usize)
+}
