26035058e18602da32330cb745c8a3e97a53a98a
QASAK <w663012911@outlook.com>
2022-12-20 18:34:38 +0800

    Update q1552_max_distance.rs
    simplify

--- src/binary_search/q1552_max_distance.rs
+++ src/binary_search/q1552_max_distance.rs
@@ -1,8 +1,8 @@
 pub fn max_distance(mut position: Vec<i32>, m: i32) -> i32 {
-    fn is_force_big(pre: &Vec<i64>, target_balls: i32, force: i32) -> bool {
+    fn is_force_big(pos: &Vec<i32>, target_balls: i32, force: i32) -> bool {
         let (mut l, mut r, mut balls) = (0, 1, 0);
-        while r < pre.len() {
-            if pre[r] - pre[l] >= force as i64 {
+        while r < pos.len() {
+            if pos[r] - pos[l] >= force {
                 l = r;
                 balls += 1;
             }
@@ -11,14 +11,10 @@ pub fn max_distance(mut position: Vec<i32>, m: i32) -> i32 {
         balls + 1 < target_balls
     }
     position.sort();
-    let mut pre = vec![0; position.len()];
-    for i in 2..=pre.len() {
-        pre[i - 1] = pre[i - 2] + (position[i - 1] - position[i - 2]) as i64;
-    }
     let (mut l, mut r) = (1, position[position.len() - 1]);
     while l < r {
         let f = l + (r - l + 1) / 2;
-        if is_force_big(&pre, m, f) {
+        if is_force_big(&position, m, f) {
             r = f - 1;
         } else {
             l = f;
