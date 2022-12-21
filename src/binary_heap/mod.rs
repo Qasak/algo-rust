5b6544cc975640c64de6ce993710d69bdb1a1e90
QASAK <w663012911@outlook.com>
2022-12-21 21:39:04 +0800

    mov

--- src/binary_heap/mod.rs
+++ src/binary_heap/mod.rs
@@ -1,3 +1,2 @@
 mod median_from_data_stream;
 mod q2503_max_point;
-mod q1753_max_score;
--- src/greedy/mod.rs
+++ src/greedy/mod.rs
@@ -4,3 +4,4 @@ mod q1710_maximum_units;
 mod q1657_close_strings;
 mod q1775_min_operations;
 mod q1703_min_moves;
+mod q1753_max_score;
