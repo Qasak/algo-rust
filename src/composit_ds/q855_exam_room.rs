d0a1723474173a56671a5da398c0eb31b69b6bb0
QASAK <w663012911@outlook.com>
2022-12-30 23:54:33 +0800

    Update q855_exam_room.rs

--- src/composit_ds/q855_exam_room.rs
+++ src/composit_ds/q855_exam_room.rs
@@ -1,41 +1,73 @@
-use std::collections::BTreeSet;
-
+use std::collections::{HashMap, HashSet, BTreeSet, BinaryHeap, BTreeMap, VecDeque};
 struct ExamRoom {
-    cap: i32,
-    seats: BTreeSet<i32>,
+    points: BTreeSet<i32>,
+    intervals: BTreeMap<i32, BTreeMap<i32, i32>>,
+    n: i32,
 }
 
 impl ExamRoom {
+
     fn new(n: i32) -> Self {
         Self {
-            cap: n,
-            seats: BTreeSet::new(),
+            points: BTreeSet::new(),
+            intervals: BTreeMap::from([(n, BTreeMap::from([(-1, n)]))]),
+            n
         }
     }
 
     fn seat(&mut self) -> i32 {
-        if self.seats.is_empty() {
-            self.seats.insert(0);
-            return 0;
-        }
-        let (mut mid, mut l, mut len) = (0, 0, 0);
-        for (i, &r) in self.seats.iter().enumerate() {
-            if i == 0 && r != 0 {
-                len = r;
-            } else if (r - l) / 2 > len{
-                len =  (r - l) / 2;
-                mid = (l + r) / 2;
+        let mut len = *self.intervals.iter().rev().next().unwrap().0;
+        if let Some(x) = self.intervals.get_mut(&len) {
+            let a = x.iter().next().unwrap();
+            let mut m = if *a.0 == -1 {0} else if *a.1 == self.n {self.n - 1} else {*a.0 + (*a.1 - *a.0) / 2};
+            let mut sub = *a.0;
+            // 偶数空位的区间x，要考虑x-1的区间
+            if len & 1 == 0 && self.n != m + 1 {
+                if let Some(x) = self.intervals.get_mut(&(len - 1)) {
+                    let mut a = x.iter().next().unwrap();
+                    let mut second = if *a.0 == -1 {0} else if *a.1 == self.n {self.n - 1} else {*a.0 + (*a.1 - *a.0) / 2};
+                    if second < m {
+                        len = len - 1;
+                        sub = *a.0;
+                        m = second;
+                    }
+                }
             }
-            l = r;
+
+            let a = sub;
+            if let Some(x) = self.intervals.get_mut(&len) {
+                if let Some(b) = x.remove(&a) {
+                    if x.is_empty() {
+                        self.intervals.remove(&len);
+                    }
+                    self.points.insert(m);
+                    self.intervals.entry(m - a - 1).or_insert(BTreeMap::new()).insert(a, m);
+                    self.intervals.entry(b - m - 1).or_insert(BTreeMap::new()).insert(m, b);
+                    return m;
+                }
+            }
         }
-        if self.cap - 1 - l > len {
-            mid = self.cap - 1;
-        }
-        self.seats.insert(mid);
-        mid
+        0
     }
 
     fn leave(&mut self, p: i32) {
-        self.seats.remove(&p);
+        let a = self.points.range(..p).rev().next().copied().unwrap_or(-1);
+        self.points.remove(&p);
+        let len = p - a - 1;
+        if let Some(x) = self.intervals.get_mut(&len) {
+            x.remove(&a);
+            if x.is_empty() {
+                self.intervals.remove(&len);
+            }
+        }
+        let b = self.points.range(p+1..).next().copied().unwrap_or(self.n);
+        let len = b - p - 1;
+        if let Some(x) = self.intervals.get_mut(&len) {
+            x.remove(&p);
+            if x.is_empty() {
+                self.intervals.remove(&len);
+            }
+        }
+        self.intervals.entry(b - a - 1).or_insert(BTreeMap::new()).insert(a, b);
     }
 }
