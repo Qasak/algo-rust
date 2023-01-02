83dc27304fbea2880a3968e8b9a51f03719fbd52
QASAK <w663012911@outlook.com>
2023-01-03 01:48:25 +0800

    Update q1801_num_orders_in_backlog.rs

--- src/binary_heap/q1801_num_orders_in_backlog.rs
+++ src/binary_heap/q1801_num_orders_in_backlog.rs
@@ -17,72 +17,70 @@ use std::cmp::Reverse;
 //     Buy: 0,
 //     Sell: 1
 // }
-impl Solution {
-    pub fn get_number_of_backlog_orders(orders: Vec<Vec<i32>>) -> i32 {
-        let mo = 1e9 as i32 + 7;
-        // q_buy: big
-        // q_sell: small
-        let (mut q_buy, mut q_sell): (BinaryHeap<(i32, i32)>, BinaryHeap<(Reverse<i32>, i32)>) = (BinaryHeap::new(), BinaryHeap::new());
-        for item in orders {
-            let (price, mut amount, order_type) = (item[0], item[1], item[2]);
-            // 0 == buy
-            if order_type == 0 {
-                if q_sell.is_empty() || q_sell.peek().unwrap().0.0 > price {
-                    q_buy.push((price, amount));
-                } else {
-                    while let Some((Reverse(sp), sa)) = q_sell.pop() {
-                        if sp > price {
-                            q_sell.push((Reverse(sp), sa));
-                            break;
-                        }
-                        if sa > amount {
-                            q_sell.push((Reverse(sp), sa - amount));
-                            amount = 0;
-                            break;
-                        } else {
-                            amount -= sa;
-                        }
+pub fn get_number_of_backlog_orders(orders: Vec<Vec<i32>>) -> i32 {
+    let mo = 1e9 as i32 + 7;
+    // q_buy: big
+    // q_sell: small
+    let (mut q_buy, mut q_sell): (BinaryHeap<(i32, i32)>, BinaryHeap<(Reverse<i32>, i32)>) = (BinaryHeap::new(), BinaryHeap::new());
+    for item in orders {
+        let (price, mut amount, order_type) = (item[0], item[1], item[2]);
+        // 0 == buy
+        if order_type == 0 {
+            if q_sell.is_empty() || q_sell.peek().unwrap().0.0 > price {
+                q_buy.push((price, amount));
+            } else {
+                while let Some((Reverse(sp), sa)) = q_sell.pop() {
+                    if sp > price {
+                        q_sell.push((Reverse(sp), sa));
+                        break;
                     }
-                    if amount > 0 {
-                        q_buy.push((price, amount));
+                    if sa > amount {
+                        q_sell.push((Reverse(sp), sa - amount));
+                        amount = 0;
+                        break;
+                    } else {
+                        amount -= sa;
                     }
                 }
-                // 1 == sell
+                if amount > 0 {
+                    q_buy.push((price, amount));
+                }
+            }
+            // 1 == sell
+        } else {
+            if q_buy.is_empty() || q_buy.peek().unwrap().0 < price {
+                q_sell.push((Reverse(price), amount));
             } else {
-                if q_buy.is_empty() || q_buy.peek().unwrap().0 < price {
+                while let Some((bp, ba)) = q_buy.pop() {
+                    if bp < price {
+                        q_buy.push((bp, ba));
+                        break;
+                    }
+                    if ba > amount {
+                        q_buy.push((bp, ba - amount));
+                        amount = 0;
+                        break;
+                    } else {
+                        amount -= ba;
+                    }
+                }
+                if amount > 0 {
                     q_sell.push((Reverse(price), amount));
-                } else {
-                    while let Some((bp, ba)) = q_buy.pop() {
-                        if bp < price {
-                            q_buy.push((bp, ba));
-                            break;
-                        }
-                        if ba > amount {
-                            q_buy.push((bp, ba - amount));
-                            amount = 0;
-                            break;
-                        } else {
-                            amount -= ba;
-                        }
-                    }
-                    if amount > 0 {
-                        q_sell.push((Reverse(price), amount));
-                    }
                 }
             }
         }
-
-
-
-        let mut ret = 0;
-        while let Some((Reverse(p), a)) = q_sell.pop() {
-            ret += a;
-            ret %= mo;
-        }
-        while let Some((p, a)) = q_buy.pop() {
-            ret += a;
-            ret %= mo;
-        }
-        ret
     }
+
+
+
+    let mut ret = 0;
+    while let Some((Reverse(p), a)) = q_sell.pop() {
+        ret += a;
+        ret %= mo;
+    }
+    while let Some((p, a)) = q_buy.pop() {
+        ret += a;
+        ret %= mo;
+    }
+    ret
 }
