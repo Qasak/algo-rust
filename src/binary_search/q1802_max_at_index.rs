a40aae208c34a5c50a5e51b66e9fff30ab81197e
QASAK <w663012911@outlook.com>
2023-01-04 18:57:12 +0800

    Update q1802_max_at_index.rs

--- src/binary_search/q1802_max_at_index.rs
+++ src/binary_search/q1802_max_at_index.rs
@@ -1,28 +1,17 @@
 
-fn check(m: i32, index: i32, n: i32) -> i64 {
-    let m = m as i64;
-    let (l, r) = (index as i64, (n - 1 - index) as i64);
-    let a1r = m - r;
-    let a1l = m - l;
-    m + if m > l {get_sn(a1l as i64, l)} else {get_sn(1, m - 1) + l - m + 1} + if m > r {get_sn(a1r as i64, r)} else {get_sn(1, m - 1) + r - m + 1}
+fn check(m: i64, index: i64, n: i64) -> i64 {
+    let (l, r) = (index, n - 1 - index);
+    m + if m > l {get_sn(m - l, l)} else {get_sn(1, m - 1) + l - m + 1} + if m > r {get_sn(m - r, r)} else {get_sn(1, m - 1) + r - m + 1}
 }
 fn get_sn(a1: i64, n: i64) -> i64 {
     a1 * n + (n - 1) * n / 2
 }
-pub fn max_value(n: i32, index: i32, max_sum: i32) -> i32 {
-    // 111111 = 6 + 1
-    // 121111 = 7 + 3
-    // 232111 = 10 + 4
-    // 343211 = 14 + 5
-    // 454321 = 19 + 6
-    // 565432 = 25 + 6
-    // 676543 = 31 + 6
 
+pub fn max_value(n: i32, index: i32, max_sum: i32) -> i32 {
     let (mut l, mut r) = (1, max_sum);
     while l < r {
         let m = l + (r - l + 1) / 2;
-        let x = check(m, index, n);
-        if x > max_sum as i64 {
+        if check(m as i64, index as i64, n as i64) > max_sum as i64 {
             r = m - 1;
         } else {
             l = m;
