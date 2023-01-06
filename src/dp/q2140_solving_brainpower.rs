8d2164e2c3a439201b14a7123e1b6e7d53e77c90
QASAK <w663012911@outlook.com>
2023-01-06 18:39:27 +0800

    Update q2140_solving_brainpower.rs

--- src/dp/q2140_solving_brainpower.rs
+++ src/dp/q2140_solving_brainpower.rs
@@ -1,18 +1,33 @@
-pub fn most_points(questions: Vec<Vec<i32>>) -> i64 {
+pub fn most_points_1(questions: Vec<Vec<i32>>) -> i64 {
     let n = questions.len();
     let mut f = vec![0i64; n];
     for i in (0..n) {
-        let (p, b) = (questions[i][0] as i64, questions[i][1]);
+        let (p, b) = (questions[i][0] as i64, questions[i][1] as usize);
         let j = i + 1;
         if j < n {
             f[j] = f[j].max(f[i]);
         }
         f[i] += p;
-        let j = i + b as usize + 1;
+        let j = i + b + 1;
         if j < n {
             f[j] = f[j].max(f[i]);
         }
         // println!("f: {:?}", f);
     }
-    *f.iter().max().unwrap()
+    f.into_iter().max().unwrap_or(0)
+}
+pub fn most_points(questions: Vec<Vec<i32>>) -> i64 {
+    let n = questions.len();
+    let mut f = vec![0i64; n];
+    f[n - 1] = questions[n - 1][0] as i64;
+    for i in (0..n - 1).rev() {
+        let (p, b) = (questions[i][0] as i64,  questions[i][1] as usize);
+        let j = i + b + 1;
+        f[i] = if j >= n {
+            p
+        } else {
+            p + f[j]
+        }.max(f[i + 1]);
+    }
+    f.into_iter().max().unwrap_or(0)
 }
