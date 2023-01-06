ac0e0d829015b38296215b2121223d884fbb3d83
QASAK <w663012911@outlook.com>
2023-01-06 18:32:19 +0800

    Update q2140_solving_brainpower.rs

---
+++ src/dp/q2140_solving_brainpower.rs
@@ -0,0 +1,18 @@
+pub fn most_points(questions: Vec<Vec<i32>>) -> i64 {
+    let n = questions.len();
+    let mut f = vec![0 as i64; n];
+    for i in (0..n) {
+        let (p, b) = (questions[i][0] as i64, questions[i][1]);
+        let j = i + 1;
+        if j < n {
+            f[j] = f[j].max(f[i]);
+        }
+        f[i] += p;
+        let j = i + b as usize + 1;
+        if j < n {
+            f[j] = f[j].max(f[i]);
+        }
+        // println!("f: {:?}", f);
+    }
+    *f.iter().max().unwrap()
+}
