cc95b519964653e209e98ccaf98511e6c78da9ae
QASAK <w663012911@outlook.com>
2023-01-09 22:28:19 +0800

    Update q1806_reinit_perm.rs

--- src/math/q1806_reinit_perm.rs
+++ src/math/q1806_reinit_perm.rs
@@ -20,3 +20,26 @@ pub fn reinitialize_permutation(n: i32) -> i32 {
     }
     ret
 }
+
+// math: find the inverse function of indexes
+// f1(i) = i / 2
+// f2(i) = n / 2 + (i - 1) / 2
+
+// g1(i) = 2 * i
+// g2(i) = 2 * i - n + 1
+pub fn reinitialize_permutation_1(n: i32) -> i32 {
+    let mut i = 1;
+    let mut ret = 0;
+    loop  {
+        ret += 1;
+        if i <= (n - 1) / 2 {
+            i = i * 2;
+        } else {
+            i = i * 2 - (n - 1);
+        }
+        if i == 1 {
+            break;
+        }
+    }
+    ret
+}
