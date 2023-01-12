887a8819ae381aab3b1d2471de527f06eb3ed1cc
QASAK <w663012911@outlook.com>
2023-01-13 01:11:10 +0800

    Update q1807_evaluate.rs

--- src/hash_map/q1807_evaluate.rs
+++ src/hash_map/q1807_evaluate.rs
@@ -48,3 +48,30 @@ pub fn evaluate_1(s: String, knowledge: Vec<Vec<String>>) -> String {
     }
     ret
 }
+
+// iter, if -> match
+pub fn evaluate_2(s: String, knowledge: Vec<Vec<String>>) -> String {
+    let (n, mut ret) = (s.len(), String::new());
+    let map = knowledge.iter().map(|v| (v[0].as_str(), v[1].as_str())).collect::<HashMap<_, _>>();
+    let mut it = s.chars();
+    while let Some(c) = it.next() {
+        match c {
+            '(' => {
+                let mut t = String::new();
+                while let Some(c) = it.next() {
+                    if c == ')' {break;}
+                    t.push(c);
+                }
+                if let Some(replace) = map.get(t.as_str()) {
+                    ret.push_str(replace);
+                } else {
+                    ret.push('?');
+                }
+            }
+            _ => {
+                ret.push(c);
+            }
+        }
+    }
+    ret
+}
