69499e4c5cefbeda47527138a978136d63e6428b
QASAK <w663012911@outlook.com>
2023-01-14 00:20:24 +0800

    add q2287(one line ver.)

--- src/one_line/mod.rs
+++ src/one_line/mod.rs
@@ -1,1 +1,2 @@
 mod q2283_digit_count;
+mod q2287_rearrange_characters;
---
+++ src/one_line/q2287_rearrange_characters.rs
@@ -0,0 +1,16 @@
+pub fn rearrange_characters(s: String, target: String) -> i32 {
+    target.bytes().fold(vec![0; 26], |mut v, b| {v[(b - b'a') as usize] += 1; v}).iter().zip(s.bytes().fold(vec![0; 26], |mut v, b| {v[(b - b'a') as usize] += 1; v}).iter()).filter(|(&i, &j)| i > 0).map(|(i, j)| j / i).min().unwrap()
+}
+
+#[cfg(test)]
+mod test {
+    use crate::one_line::q2287_rearrange_characters::rearrange_characters;
+
+    #[test]
+    fn f() {
+        let s = "lrnvlcqukanpdnluowenfxquitzryponxsikhciohyostvmkapkfpglzikitwiraqgchxnpryhwpuwpozacjhmwhjvslprqlnxrk".to_owned();
+        let target = "woijih".to_owned();
+        assert_eq!(2, rearrange_characters(s, target));
+
+    }
+}
