d923a1d6adc89bf2bebb3b81ca983bde9287cc21
QASAK <w663012911@outlook.com>
2023-01-19 02:16:27 +0800

    Update q1825_mk_avg.rs

--- src/ordered_collection/q1825_mk_avg.rs
+++ src/ordered_collection/q1825_mk_avg.rs
@@ -1,5 +1,7 @@
 use std::collections::{BTreeMap, VecDeque};
+#[derive(Debug)]
 struct MultiSet(BTreeMap<i32, i32>);
+// MultiSet: BTreeSet但是可以有重复值
 impl MultiSet {
     pub fn new() -> Self {
         MultiSet(BTreeMap::new())
@@ -64,6 +66,8 @@ impl MKAverage {
     }
 
     fn add_element(&mut self, cur: i32) {
+
+        // cur加入dq
         self.dq.push_back(cur);
         if self.dq.len() <= self.m as usize {
             self.mid.insert(cur);
@@ -73,6 +77,7 @@ impl MKAverage {
                     //safe m > 2 * k
                     // 拿走mid的前后k个值
                     // 维护mid的总和sum
+                    // 将拿走的值分别放到l, r
                     let first = self.mid.first();
                     self.mid.remove(first);
                     self.sum -= first as i64;
@@ -84,42 +89,49 @@ impl MKAverage {
                     self.r.insert(last);
                 }
             }
-            return;
-        }
-        //len == m+1
-        let l_last = self.l.last();
-        let r_first = self.r.first();
-        if cur < l_last {
-            self.l.insert(cur);
-            self.l.remove(l_last);
-            self.mid.insert(l_last);
-            self.sum += l_last as i64;
-        } else if cur > r_first {
-            self.r.insert(cur);
-            self.r.remove(r_first);
-            self.mid.insert(r_first);
-            self.sum += r_first as i64;
         } else {
-            self.mid.insert(cur);
-            self.sum += cur as i64;
+            //len == m+1
+            // 将cur插入合适的set
+            // 让左右两个set始终保持k个数，中间多一个数
+            let l_last = self.l.last();
+            let r_first = self.r.first();
+            if cur < l_last {
+                self.l.insert(cur);
+                self.l.remove(l_last);
+                self.mid.insert(l_last);
+                self.sum += l_last as i64;
+            } else if cur > r_first {
+                self.r.insert(cur);
+                self.r.remove(r_first);
+                self.mid.insert(r_first);
+                self.sum += r_first as i64;
+            } else {
+                self.mid.insert(cur);
+                self.sum += cur as i64;
+            }
+
+            // 维护dq大小为m, 删除最先(最左)插入的数temp
+            let temp = self.dq.pop_front().unwrap();
+            // 维护三个set, 找到删除的数temp的位置并删除
+            // 删除的位置在左右，那么从中间移一个数过去
+            if self.l.contains(temp) {
+                self.l.remove(temp);
+                let first = self.mid.first();
+                self.mid.remove(first);
+                self.sum -= first as i64;
+                self.l.insert(first);
+            } else if self.r.contains(temp) {
+                self.r.remove(temp);
+                let last = self.mid.last();
+                self.mid.remove(last);
+                self.sum -= last as i64;
+                self.r.insert(last);
+            } else {
+                self.mid.remove(temp);
+                self.sum -= temp as i64;
+            }
         }
-        let temp = self.dq.pop_front().unwrap();
-        if self.l.contains(temp) {
-            self.l.remove(temp);
-            let first = self.mid.first();
-            self.mid.remove(first);
-            self.sum -= first as i64;
-            self.l.insert(first);
-        } else if self.r.contains(temp) {
-            self.r.remove(temp);
-            let last = self.mid.last();
-            self.mid.remove(last);
-            self.sum -= last as i64;
-            self.r.insert(last);
-        } else {
-            self.mid.remove(temp);
-            self.sum -= temp as i64;
-        }
+
     }
 
     fn calculate_mk_average(&self) -> i32 {
