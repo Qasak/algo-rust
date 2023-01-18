3bc1c56c63dde0752b0d8a8e67f2cfb5e6525819
QASAK <w663012911@outlook.com>
2023-01-18 15:56:43 +0800

    Create s3_lifetime.rs

---
+++ src/sys/s3_lifetime.rs
@@ -0,0 +1,84 @@
+use std::rc::Rc;
+use std::sync::{Arc, RwLock};
+
+#[test]
+fn arc_rwlock_test() {
+    let s = Arc::new(RwLock::new(format!("{}", "hello from fff")));
+    let t = s.clone();
+    std::thread::spawn(move || {
+        t.as_ref().write().unwrap().push_str(" asd");
+        println!("from thread {:?}",t.as_ref().read().unwrap());
+    }).join().unwrap();
+
+    println!("main : {:?}", s.as_ref().read().unwrap());
+}
+
+#[test]
+fn rc_test_1() {
+    let a = Rc::new(String::from("hello, world"));
+    let b = a.clone();
+
+    assert_eq!(2, Rc::strong_count(&a));
+    assert_eq!(Rc::strong_count(&a), Rc::strong_count(&b))
+}
+
+#[test]
+fn string_test() {
+    let b = foo("world");
+    println!("{}", b);
+}
+
+fn foo(x: &str) -> String {
+    let a = "Hello, ".to_string() + x;
+    a
+}
+
+#[test]
+fn addr_test() {
+    let data = vec![1, 2, 3, 4];
+    let data1 = &data;
+    // 值的地址是什么？引用的地址又是什么？
+    println!(
+        "addr of value: {:p}({:p}), addr of data {:p}, data1: {:p}",
+        &data, data1, &*data, &data1
+    );
+    println!("sum of data1: {}", sum(data1));
+
+    // 堆上数据的地址是什么？
+    println!(
+        "addr of items: [{:p}, {:p}, {:p}, {:p}]",
+        &data[0], &data[1], &data[2], &data[3]
+    );
+}
+
+fn sum(data: &Vec<u32>) -> u32 {
+    // 值的地址会改变么？引用的地址会改变么？
+    println!("addr of value: {:p}, addr of ref: {:p}", data, &data);
+    data.iter().fold(0, |acc, x| acc + x)
+}
+
+#[test]
+fn test_strtok() {
+    let s = "hello world".to_owned();
+    let mut s1 = s.as_str();
+    let hello = strtok(&mut s1, ' ');
+    // println!("hello is: {}, s1: {}, s: {}", hello, s1, s);
+    println!("hello is: {}, s: {}", hello, s);
+    println!("s1: {}", s1);
+}
+
+pub fn strtok<'a>(s: &'a mut &str, delimiter: char) -> &'a str {
+    if let Some(i) = s.find(delimiter) {
+        let prefix = &s[..i];
+        // 由于 delimiter 可以是 utf8，所以我们需要获得其 utf8 长度，
+        // 直接使用 len 返回的是字节长度，会有问题
+        let suffix = &s[(i + delimiter.len_utf8())..];
+        *s = suffix;
+        prefix
+    } else { // 如果没找到，返回整个字符串，把原字符串指针 s 指向空串
+        let prefix = *s;
+        *s = "";
+        prefix
+    }
+}
+
