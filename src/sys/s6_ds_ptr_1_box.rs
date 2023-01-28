c919dfd14c18c713baa0d9220ce9d688451bc0ac
QASAK <w663012911@outlook.com>
2023-01-28 22:24:27 +0800

    Update s6_ds_ptr_1_box.rs
    System.alloc(layout); bug in apple M series
    //  (signal: 4, SIGILL: illegal instruction)
    //  Process finished with exit code 101

--- src/sys/s6_ds_ptr_1_box.rs
+++ src/sys/s6_ds_ptr_1_box.rs
@@ -1,39 +1,41 @@
-
-use std::alloc::{GlobalAlloc, Layout, System};
-
-struct MyAllocator;
-
-unsafe impl GlobalAlloc for MyAllocator {
-    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        let data = System.alloc(layout);
-        format!("ALLOC: {:p}, size {}", data, layout.size());
-        data
-    }
-
-    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-        System.dealloc(ptr, layout);
-        format!("FREE: {:p}, size {}", ptr, layout.size());
-    }
-}
-
-#[global_allocator]
-static GLOBAL: MyAllocator = MyAllocator;
-
-#[allow(dead_code)]
-struct Matrix {
-    // 使用不规则的数字如 505 可以让 dbg! 的打印很容易分辨出来
-    data: [u8; 505],
-}
-
-impl Default for Matrix {
-    fn default() -> Self {
-        Self { data: [0; 505] }
-    }
-}
-
-
 //  (signal: 4, SIGILL: illegal instruction)
-// Process finished with exit code 101
+//  Process finished with exit code 101
+
+
+// use std::alloc::{GlobalAlloc, Layout, System};
+//
+// struct MyAllocator;
+//
+// unsafe impl GlobalAlloc for MyAllocator {
+//     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+//         let data = System.alloc(layout);
+//         format!("ALLOC: {:p}, size {}", data, layout.size());
+//         data
+//     }
+//
+//     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+//         System.dealloc(ptr, layout);
+//         format!("FREE: {:p}, size {}", ptr, layout.size());
+//     }
+// }
+//
+// #[global_allocator]
+// static GLOBAL: MyAllocator = MyAllocator;
+//
+// #[allow(dead_code)]
+// struct Matrix {
+//     // 使用不规则的数字如 505 可以让 dbg! 的打印很容易分辨出来
+//     data: [u8; 505],
+// }
+//
+// impl Default for Matrix {
+//     fn default() -> Self {
+//         Self { data: [0; 505] }
+//     }
+// }
+
+
+
 #[test]
 fn my_allocator_test() {
     // let data = Box::new(Matrix::default());
