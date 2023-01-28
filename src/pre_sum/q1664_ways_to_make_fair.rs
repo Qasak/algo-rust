53519ae40176d2cf7be94d55d1885fb7a20cbadb
QASAK <w663012911@outlook.com>
2023-01-28 17:21:22 +0800

    Update q1664_ways_to_make_fair.rs
    simple

--- src/pre_sum/q1664_ways_to_make_fair.rs
+++ src/pre_sum/q1664_ways_to_make_fair.rs
@@ -31,3 +31,24 @@ pub fn ways_to_make_fair(nums: Vec<i32>) -> i32 {
     }
     ret
 }
+
+// simplify version
+pub fn ways_to_make_fair_1(nums: Vec<i32>) -> i32 {
+    let n = nums.len();
+    let (o_sum, e_sum) = (0..n).fold((0, 0), |(odd, even), i| if i & 1 == 0 {(odd, even + nums[i])} else {(odd + nums[i], even)});
+    (0..n).fold((0, 0, 0), |(ret, o_pre, e_pre), i|
+        if i & 1 == 0 {
+            if e_pre + (o_sum - o_pre) == o_pre + (e_sum - e_pre - nums[i]) {
+                (ret + 1, o_pre, e_pre + nums[i])
+            } else {
+                (ret, o_pre, e_pre + nums[i])
+            }
+        }  else {
+            if  o_pre + (e_sum - e_pre) == e_pre + (o_sum - o_pre - nums[i]) {
+                (ret + 1, o_pre + nums[i], e_pre)
+            } else {
+                (ret, o_pre + nums[i], e_pre)
+            }
+        }
+    ).0
+}
