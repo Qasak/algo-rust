5092f6426f90f7b4598124086c03d9147b4b6f4b
QASAK <w663012911@outlook.com>
2023-01-28 22:37:35 +0800

    Update s6_ds_ptr_3_mutex_guard.rs
    use once_cell::sync::Lazy to replace lazy_static!

--- src/sys/s6_ds_ptr_3_mutex_guard.rs
+++ src/sys/s6_ds_ptr_3_mutex_guard.rs
@@ -1,4 +1,4 @@
-use std::borrow::Cow;
+use std::borrow::{Borrow, Cow};
 use std::collections::HashMap;
 use std::ops::DerefMut;
 use std::os::unix::raw::mode_t;
@@ -6,17 +6,21 @@ use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 use lazy_static::lazy_static;
+use once_cell::sync::Lazy;
 
-lazy_static!{
-    static ref Metrics: Mutex<HashMap<Cow<'static, str>, usize>> =
-        Mutex::new(HashMap::new());
-}
+// lazy_static!{
+//     static ref METRICS: Mutex<HashMap<Cow<'static, str>, usize>> =
+//         Mutex::new(HashMap::new());
+// }
+
+static METRICS: Lazy<Mutex<HashMap<Cow<'static, str>, usize>>> = Lazy::new(|| Mutex::new(HashMap::new()));
 
 #[test]
 fn f() {
-    let metrics: Arc<Mutex<HashMap<Cow<'static, str>, usize>>> = Arc::new(Mutex::new(HashMap::new()));
+    // let metrics: Arc<Mutex<HashMap<Cow<'static, str>, usize>>> = Arc::new(Mutex::new(HashMap::new()));
     for _ in 0..32 {
-        let m = metrics.clone();
+        // let m = metrics.clone();
+        let m = &METRICS;
         thread::spawn(move || {
             let mut g = m.lock().unwrap();
             let data = &mut *g;
@@ -26,5 +30,7 @@ fn f() {
         });
     }
     thread::sleep(Duration::from_millis(100));
-    println!("metrics: {:?}", metrics.lock().unwrap());
+    println!("metrics: {:?}", METRICS.lock().unwrap());
+    // println!("metrics: {:?}", metrics.lock().unwrap());
+
 }
