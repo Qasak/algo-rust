8a056c37e94db94355328052bcc5ccf49051d7fb
QASAK <w663012911@outlook.com>
2023-01-30 23:19:00 +0800

    Update mod.rs

--- src/linked_list/mod.rs
+++ src/linked_list/mod.rs
@@ -4,3 +4,21 @@ mod d_list;
 mod q141_has_cycle;
 mod q25_k_reverse;
 mod q206_reverse_linked_list;
+mod q1669_merge_linked_list;
+
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>
+}
+
+impl ListNode {
+    #[inline]
+    pub(crate) fn new(val: i32) -> Self {
+        ListNode {
+            next: None,
+            val
+        }
+    }
+}
