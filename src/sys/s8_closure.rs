e122e64f438da6c606721eb488acef771f2fa7cf
QASAK <w663012911@outlook.com>
2023-02-01 12:38:17 +0800

    Update s8_closure.rs
    closure trait

--- src/sys/s8_closure.rs
+++ src/sys/s8_closure.rs
@@ -194,3 +194,50 @@ fn test_closure_lifetime() {
     c();
     // 请问在这里，还能访问 name 么？为什么？
 }
+
+
+
+pub trait Executor {
+    fn execute(&self, cmd: &str) -> Result<String, &'static str>;
+}
+
+struct BashExecutor {
+    env: String,
+}
+
+impl Executor for BashExecutor {
+    fn execute(&self, cmd: &str) -> Result<String, &'static str> {
+        Ok(format!(
+            "fake bash execute: env: {}, cmd: {}",
+            self.env, cmd
+        ))
+    }
+}
+
+impl<F> Executor for F
+    where
+        F: Fn(&str) -> Result<String, &'static str>
+{
+    fn execute(&self, cmd: &str) -> Result<String, &'static str> {
+        self(cmd)
+    }
+}
+
+// line let r2 = execute(...);
+#[test]
+fn closure_trait() {
+    let env = "PATH=/usr/bin".to_string();
+
+    let cmd = "cat /etc/passwd";
+    let r1 = execute(cmd, BashExecutor { env: env.clone() });
+    println!("{:?}", r1);
+
+    let r2 = execute(cmd, |cmd: &str| {
+        Ok(format!("fake fish execute: env: {}, cmd: {}", env, cmd))
+    });
+    println!("{:?}", r2);
+}
+
+fn execute(cmd: &str, exec: impl Executor) -> Result<String, &'static str> {
+    exec.execute(cmd)
+}
