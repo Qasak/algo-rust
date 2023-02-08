be29990e8cc1763ede17625a4a58b2bf6b3ba7d2
QASAK <w663012911@outlook.com>
2023-02-08 20:20:39 +0800

    Update q206_reverse_linked_list.rs
    add recursive ver

--- src/linked_list/q206_reverse_linked_list.rs
+++ src/linked_list/q206_reverse_linked_list.rs
@@ -1,13 +1,42 @@
 use crate::linked_list::ListNode;
 
+
 pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-    let mut cur = head;
-    let mut pre = None;
-    while let Some(mut cur_node) = cur {
-        let nex = cur_node.next;
-        cur_node.next = pre;
-        pre = Some(cur_node);
-        cur = nex;
+    // a: prev, b: curr, c: next
+    let mut b = head;
+    let mut a = None;
+    while let Some(mut bb) = b {
+        let c = bb.next.take();
+        bb.next = a;
+        a = Some(bb);
+        b = c;
     }
-    pre
+    a
+}
+
+type OptNode = Option<Box<ListNode>>;
+pub fn reverse_list_rec(head: OptNode) -> OptNode {
+    // a: prev, b: curr, c: next
+    fn reverse(a: OptNode, b: OptNode) -> OptNode {
+        if let Some(mut bb) = b {
+            let c = bb.next.take();
+            bb.next = a;
+            reverse(Some(bb), c)
+        } else {
+            a
+        }
+    }
+    reverse(None, head)
+}
+
+
+#[test]
+fn test() {
+    let v = vec![1,2,3,4,5];
+    let vv = v.clone();
+    let list1: ListNode = vv.into();
+    list1.print_list();
+    let list: ListNode = v.into();
+    let new_head = reverse_list(Some(Box::new(list)));
+    new_head.unwrap().print_list();
 }
