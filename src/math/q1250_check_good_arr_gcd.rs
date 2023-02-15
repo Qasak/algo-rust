9196dd47191a143d20499a1c66c219a97edd3508
QASAK <w663012911@outlook.com>
2023-02-15 23:33:20 +0800

    Update q1250_check_good_arr_gcd.rs

--- src/math/q1250_check_good_arr_gcd.rs
+++ src/math/q1250_check_good_arr_gcd.rs
@@ -1,12 +1,15 @@
 use std::ops::Rem;
-fn is_good_array(nums: Vec<i32>) -> bool {
-    fn gcd<T: Rem<Output = T> + std::cmp::PartialOrd + From<u8> + Copy>(a: T, b: T) -> T {
-        if b == T::from(0) {
-            a
-        } else {
-            gcd(b, a % b)
-        }
+
+fn gcd<T: Rem<Output = T> + std::cmp::PartialOrd + From<u8> + Copy>(a: T, b: T) -> T {
+    if b == T::from(0) {
+        a
+    } else {
+        gcd(b, a % b)
     }
+}
+
+fn is_good_array(nums: Vec<i32>) -> bool {
+
     // 计算所有数的最大公因数
     let mut g = nums[0];
     for i in 1..nums.len() {
