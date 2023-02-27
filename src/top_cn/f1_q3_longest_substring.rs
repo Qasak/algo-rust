a0032a2a853240be2946d0cac57a823374142a5f
QASAK <w663012911@outlook.com>
2023-02-27 13:33:31 +0800

    Update f1_q3_longest_substring.rs

--- src/top_cn/f1_q3_longest_substring.rs
+++ src/top_cn/f1_q3_longest_substring.rs
@@ -1,39 +1,19 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
+
 
 pub fn length_of_longest_substring(s: String) -> i32 {
-    let mut set = HashSet::new();
-    let cs = s.chars().collect::<Vec<char>>();
-    let mut l = 0;
-    let n = s.len();
-    let mut ret = 0;
-    for i in 0..n {
-        if !set.contains(&cs[i]) {
-            set.insert(cs[i]);
-        } else {
-            while l < i && cs[l] != cs[i] {
-                set.remove(&cs[l]);
-                l += 1;
-            }
-            l += 1;
-        }
-        ret = ret.max(set.len())
+    if s.len() == 0 {
+        return 0;
     }
-    ret as i32
-}
-
-pub fn length_of_longest_substring_1(s: String) -> i32 {
-    let n = s.len();
-    let mut left: Vec<i32> = vec![-1; 128];
-    let s = s.chars().collect::<Vec<char>>();
-    let mut l = -1;
-    let mut ret = 0;
-    for r in 0..n {
-        // maintain left bound
-        l = l.max(left[s[r] as usize]);
-        // cur len
-        ret = ret.max(r as i32 - l);
-        // update current item's last index
-        left[s[r] as usize] = r as i32;
+    let mut map = HashMap::new();
+    let mut start = 0;
+    let mut ret = 1;
+    for (i, c) in s.chars().enumerate() {
+        if let Some(&j) = map.get(&c) {
+            start = start.max(j + 1);
+        }
+        map.insert(c, i);
+        ret = ret.max(i - start + 1);
     }
     ret as i32
 }
