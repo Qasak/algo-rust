5e9851f23c9b46fd7a8426b5bf78b1118166a8b4
QASAK <w663012911@outlook.com>
2023-03-02 01:45:50 +0800

    Update f4_q215_kth_largest_elem_in_array.rs

--- src/top_cn/f4_q215_kth_largest_elem_in_array.rs
+++ src/top_cn/f4_q215_kth_largest_elem_in_array.rs
@@ -1,70 +1,35 @@
 use rand::Rng;
 
-// heap solution
-pub fn find_kth_largest_heap(nums: Vec<i32>, k: i32) -> i32 {
-    use std::collections::BinaryHeap;
-    let mut q = BinaryHeap::new();
-    nums.iter().for_each(|&i| q.push(i));
-    (1..k).into_iter().for_each(|_| {
-        q.pop();
-    });
-    *q.peek().unwrap()
+pub fn find_kth_largest(mut nums: Vec<i32>, k: i32) -> i32 {
+    let n = nums.len();
+    let k = n - k as usize;
+    quickselect(&mut nums, k)
 }
 
-pub fn find_kth_largest(mut nums: Vec<i32>, k: i32) -> i32 {
-    fn qss(arr: &mut Vec<i32>, l: usize, r: usize, k: usize) {
-        if l >= r {
-            return;
-        }
-        let (le, eq) = partition(arr, l, r);
-        // usize overflow
-        if k as i32 <= (le as i32 - 1) {
-            qss(arr, l, le - 1, k);
-        }
-        if k >= eq + 1 {
-            qss(arr, eq + 1, r, k);
-        }
+fn quickselect<T: Ord + Copy>(arr: &mut [T], k: usize) -> T {
+    let len = arr.len();
+    let pivot = partition(arr);
+    match pivot.cmp(&k) {
+        std::cmp::Ordering::Equal => arr[pivot],
+        std::cmp::Ordering::Greater => quickselect(&mut arr[0..pivot], k),
+        std::cmp::Ordering::Less => quickselect(&mut arr[pivot + 1..len], k - pivot - 1),
     }
-    fn partition(arr: &mut Vec<i32>, l: usize, r: usize) -> (usize, usize) {
-        let mut rng = rand::thread_rng();
-        // if use rand 0.8.5
-        // let pick = rng.gen_range(l..r);
-        let pick = rng.gen_range(l, r + 1);
-        let pivot = arr[pick];
-        swap(arr, pick, r);
-        // le: next pos of less/ first item equal to pivot
-        // eq: next pos of eq/ first item greater to pivot
-        // gt: left next pos of gt/ last item equal to pivot
-        let (mut le, mut eq, mut gt) = (l, l, r - 1);
-        while gt > 0 && eq <= gt {
-            if arr[eq] == pivot {
-                eq += 1;
-            } else if arr[eq] < pivot {
-                swap(arr, eq, le);
-                eq += 1;
-                le += 1;
-            } else {
-                swap(arr, eq, gt);
-                // usize overflow
-                if gt == 0 {
-                    break;
-                }
-                gt -= 1;
-            }
-        }
+}
 
-        swap(arr, eq, r);
-        (le, eq)
+fn partition<T: Ord + Copy>(arr: &mut [T]) -> usize {
+    let mut rng = rand::thread_rng();
+    let len = arr.len();
+    let pivot_index = rng.gen_range(0, len);
+    arr.swap(pivot_index, len - 1);
+    let mut i = 0;
+    for j in 0..len - 1 {
+        if arr[j] <= arr[len - 1] {
+            arr.swap(i, j);
+            i += 1;
+        }
     }
-    fn swap(arr: &mut Vec<i32>, i: usize, j: usize) {
-        let t = arr[i];
-        arr[i] = arr[j];
-        arr[j] = t;
-    }
-    let k = k as usize;
-    let n = nums.len();
-    qss(&mut nums, 0, n - 1, n - k);
-    nums[n - k]
+    arr.swap(i, len - 1);
+    i
 }
 
 #[cfg(test)]
