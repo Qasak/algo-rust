8689e260427511915148e0fb7717bd9145db11d6
QASAK <w663012911@outlook.com>
2023-03-04 01:50:50 +0800

    Update f9_q21_merge_two_lists.rs

--- src/top_cn/f9_q21_merge_two_lists.rs
+++ src/top_cn/f9_q21_merge_two_lists.rs
@@ -18,3 +18,55 @@ pub fn merge_two_lists(
         }
     }
 }
+
+pub fn merge_two_lists_iter(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    let mut dummy_head = ListNode::new(0);
+    let mut tail = &mut dummy_head;
+    let (mut p1, mut p2) = (l1, l2);
+
+    while p1.is_some() && p2.is_some() {
+        let mut n1 = p1.unwrap();
+        let mut n2 = p2.unwrap();
+        if n1.val < n2.val {
+            let next = n1.next.take();
+            tail.next = Some(n1);
+            p1 = next;
+            p2 = Some(n2);
+        } else {
+            let next = n2.next.take();
+            tail.next = Some(n2);
+            p2 = next;
+            p1 = Some(n1);
+        }
+        tail = tail.next.as_mut().unwrap();
+
+    }
+
+    if p1.is_some() {
+        tail.next = p1;
+    }
+    if p2.is_some() {
+        tail.next = p2;
+    }
+
+    dummy_head.next
+}
+#[test]
+fn test_merge() {
+    let l1 = Some(Box::new(ListNode {
+        val: 1,
+        next: Some(Box::new(ListNode {
+            val: 2,
+            next: Some(Box::new(ListNode { val: 4, next: None })),
+        })),
+    }));
+    let l2 = Some(Box::new(ListNode {
+        val: 1,
+        next: Some(Box::new(ListNode {
+            val: 3,
+            next: Some(Box::new(ListNode { val: 4, next: None })),
+        })),
+    }));
+    let merged = merge_two_lists_iter(l1, l2);
+    ListNode::print_list(&merged);
+}
