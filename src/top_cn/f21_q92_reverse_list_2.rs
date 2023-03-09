1fde7812afa6e986dd825e2d1152bbb79204320b
QASAK <w663012911@outlook.com>
2023-03-09 20:09:24 +0800

    Update f21_q92_reverse_list_2.rs
    general reverse func

--- src/top_cn/f21_q92_reverse_list_2.rs
+++ src/top_cn/f21_q92_reverse_list_2.rs
@@ -1,6 +1,10 @@
 use crate::linked_list::ListNode;
 
-pub fn reverse_between(head: Option<Box<ListNode>>, left: i32, right: i32) -> Option<Box<ListNode>> {
+pub fn reverse_between(
+    head: Option<Box<ListNode>>,
+    left: i32,
+    right: i32,
+) -> Option<Box<ListNode>> {
     let mut dummy = ListNode::new(0);
     dummy.next = head;
     let mut ptr = &mut dummy;
@@ -9,32 +13,21 @@ pub fn reverse_between(head: Option<Box<ListNode>>, left: i32, right: i32) -> Op
         ptr = ptr.next.as_mut().unwrap();
     }
 
-
-
-    if let Some(mut pre_node) = ptr.next.take() {
-        let mut cur = pre_node.next.take();
-
-        for _ in left..right {
-            let mut cur_node = cur.unwrap();
-            cur = cur_node.next.replace(pre_node);
-            pre_node = cur_node;
+    let mut cur = ptr.next.take();
+    let mut pre = None;
+    for _ in left..=right {
+        if let Some(mut node) = cur {
+            let next = node.next.take();
+            node.next = pre;
+            pre = Some(node);
+            cur = next;
         }
-
-        // for _ in left..right {
-        //     let mut cur_node = cur.unwrap();
-        //     let next = cur_node.next.take();
-        //     cur_node.next = Some(pre_node);
-        //     pre_node = cur_node;
-        //     cur = next;
-        // }
-
-        ptr.next = Some(pre_node);
-
-        for _ in left..=right {
-            ptr = ptr.next.as_mut().unwrap();
-        }
-
-        ptr.next = cur;
     }
+    ptr.next = pre;
+    for _ in left..=right {
+        ptr = ptr.next.as_mut().unwrap();
+    }
+
+    ptr.next = cur;
     dummy.next
 }
