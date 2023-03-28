6b8ddc888861b32deb88b5352611f9a5fd63a435
QASAK <w663012911@outlook.com>
2023-03-28 22:11:30 +0800

    Update q212_word_search_ii.rs

--- src/back_track/q212_word_search_ii.rs
+++ src/back_track/q212_word_search_ii.rs
@@ -1,8 +1,15 @@
 use std::collections::{HashMap, HashSet};
-
 // naive backtrack: TLE
 pub fn find_words(board: Vec<Vec<char>>, words: Vec<String>) -> Vec<String> {
-    fn dfs(word: &String, i: usize, x: usize, y: usize, board: &Vec<Vec<char>>, ret: &mut HashSet<String>, vis: &mut HashSet<(usize, usize)>) {
+    fn dfs(
+        word: &String,
+        i: usize,
+        x: usize,
+        y: usize,
+        board: &Vec<Vec<char>>,
+        ret: &mut HashSet<String>,
+        vis: &mut HashSet<(usize, usize)>,
+    ) {
         if board[x][y] != word.chars().nth(i).unwrap() {
             return;
         }
@@ -10,8 +17,18 @@ pub fn find_words(board: Vec<Vec<char>>, words: Vec<String>) -> Vec<String> {
             ret.insert(word.clone());
             return;
         }
-        for (xx, yy) in [(x - 1, y), (x, y + 1), (x + 1, y), (x, y - 1)] {
-            if xx >= 0 && xx < board.len() && yy >= 0 && yy < board[0].len() && !vis.contains(&(xx, yy)) {
+        for (xx, yy) in [
+            (x.wrapping_sub(1), y),
+            (x, y + 1),
+            (x + 1, y),
+            (x, y.wrapping_sub(1)),
+        ] {
+            if xx >= 0
+                && xx < board.len()
+                && yy >= 0
+                && yy < board[0].len()
+                && !vis.contains(&(xx, yy))
+            {
                 vis.insert((xx, yy));
                 dfs(word, i + 1, xx, yy, board, ret, vis);
                 vis.remove(&(xx, yy));
@@ -31,4 +48,24 @@ pub fn find_words(board: Vec<Vec<char>>, words: Vec<String>) -> Vec<String> {
         }
     }
     ret.into_iter().collect::<Vec<String>>()
+}
+#[cfg(test)]
+mod test {
+    use super::find_words;
+    #[test]
+    fn f() {
+        let board = vec![
+            vec!['o', 'a', 'a', 'n'],
+            vec!['e', 't', 'a', 'e'],
+            vec!['i', 'h', 'k', 'r'],
+            vec!['i', 'f', 'l', 'v'],
+        ];
+        let words = vec![
+            "oath".to_owned(),
+            "pea".to_owned(),
+            "eat".to_owned(),
+            "rain".to_owned(),
+        ];
+        find_words(board, words);
+    }
 }
