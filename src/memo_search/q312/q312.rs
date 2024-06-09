4761241c60efa2b5f20e7443fefc596712b06c12
qasak <w663012911@outlook.com>
2024-06-10 02:13:53 +0900

    Update q312.rs

--- src/memo_search/q312/q312.rs
+++ src/memo_search/q312/q312.rs
@@ -1,4 +1,5 @@
 use std::cmp::max;
+// 观察戳气球的操作，发现这会导致两个气球从不相邻变成相邻，使得后续操作难以处理。于是我们倒过来看这些操作，将全过程看作是每次添加一个气球。
 fn dfs(l: usize, r: usize, nums: &Vec<i32>, cache: &mut Vec<Vec<i32>>) -> i32 {
     if l >= r - 1 {
         return 0;
