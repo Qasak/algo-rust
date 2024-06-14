3b8b1915b12aca9c2b3f782035ec0050c9e7257d
QASAK <w663012911@outlook.com>
2024-06-14 15:27:44 +0800

    Create q115.rs

---
+++ src/memo_search/q115/q115.rs
@@ -0,0 +1,44 @@
+impl Solution {
+    // n = s.len(), m = t.len()
+    // i指向s, j指向t
+    // 一个方案指t完全匹配，即j==m  返回1
+    // i = n, j < m 则说明该方案匹配不上 返回0
+    // 用s来匹配t
+    // s[i]==t[j]时可以选择匹配或不匹配
+    // 匹配：i += 1, j += 1
+    // 不匹配：i += 1, j
+    // s[i]!=t[j]时
+    // i += 1, j
+    pub fn num_distinct(s: String, t: String) -> i32 {
+        let ss: Vec<char> = s.chars().collect();
+        let tt: Vec<char> = t.chars().collect();
+        let (n, m) = (ss.len(), tt.len());
+        let mut cache = vec![vec![-1; m]; n];
+
+        if n < m {
+            return 0;
+        }
+        fn dfs(i: usize, j: usize, ss: &Vec<char>, tt: &Vec<char>, cache: &mut Vec<Vec<i32>>) -> i32{
+            let (n, m) = (ss.len(), tt.len());
+
+            // 包含i == n，j == m 的情况
+            if j == m {
+                return 1;
+            }
+            if i == n {
+                return 0;
+            }
+            // 记忆化
+            if cache[i][j] != -1 {
+                return cache[i][j];
+            }
+            let mut cnt = dfs(i + 1, j, ss, tt, cache);
+            if ss[i] == tt[j] {
+                cnt += dfs(i + 1, j + 1, ss, tt, cache);
+            } 
+            cache[i][j] = cnt;
+            cnt
+        }
+        dfs(0, 0, &ss, &tt, &mut cache)
+    }
+}
